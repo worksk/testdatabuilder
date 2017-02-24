16320d0576c0924eb2b24b88d561e2bc44007c9d
sunke <sunke@gbase.cn>
2017-02-24 11:30:44 +0800

    初次代码结构提交

---
+++ src/main/java/cn/shujuguan/ot/databuild/Main.java
@@ -0,0 +1,14 @@
+package cn.shujuguan.ot.databuild;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+	
+	public static void main(String[] args) {
+ 
+	}
+
+}
